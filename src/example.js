39e67e8880a321a39e8e075293469f4ab69fc290
Vladyslav Holovashchenko <holovashcenko@gmail.com>
2022-03-30 12:39:14 +0300

    Add SCSS, Assets, Babel

---
+++ src/example.js
@@ -0,0 +1,5 @@
+export default {
+  name: 'Vlad',
+  age: 32,
+  isMarried: true,
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const someObj = {
-  name: 'Vlad',
-  age: 32,
-  isMarried: true,
-}
+import someObj from './example'
+import './styles/main.scss'
+import exampleImg from './assets/example.svg'
+
+const exlImg = document.getElementById('exlImg')
+exlImg.src = exampleImg
+exlImg.width = '200'
 
 console.log(someObj)
